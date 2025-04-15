28c90e58e41d7688e304461d78cc6980443dda42
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-15 15:58:41 +0500

    Update main.py

--- movie-tracker/main.py
+++ movie-tracker/main.py
@@ -9,7 +9,8 @@ choices = st.sidebar.selectbox("view movies", menue)
 
 if choices == "add movie":
     st.subheader("Add your fvt movie")
-elif
+elif choices == "view movie":
+    st.subheader("you have viewed")
 
 
 
